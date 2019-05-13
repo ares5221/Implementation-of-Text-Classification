d8645859b8253a36a9c25f46479b5428df08dc51
liujiefeiAILAB <674361437@qq.com>
2019-05-13 12:46:46 +0800

    text similary

---
+++ 04Text-Similarity/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+from text_similarity import cosSim
+
+a = cosSim()
+r = a.CalcuSim(["你好奥众，今天是星期三", "你好奥迪，今天是星期五"])
+print(r)
+r = a.CalcuSim(["你好奥众，今天是星期三", "你好大众，今天是星期四"])
+print(r)
+
+r = a.CalcuSim(["以下是README的一些说明，我们可以看到模型的识别效果总体还是不错的", "我们可以看到模型的识别效果总体还是不错的，以下是README的一些说明"])
+print(r)
