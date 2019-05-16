d77b2db49c18e92745136126141e6b64a1bf4183
liujiefeiAILAB <674361437@qq.com>
2019-05-16 17:52:42 +0800

    bug

--- 05ReadLabelSaveinExcel/read_ann_files.py
+++ 05ReadLabelSaveinExcel/read_ann_files.py
@@ -33,142 +33,242 @@ def read_ann():
 
 def convert_label2value(label_list):
     len_data = len(label_list)
-    #攻击行为/违纪行为/品行问题/不良嗜好/退缩/抑郁问题/焦虑问题/自我中心/学习问题/极端事件/
-    # 性别/年级/健康状况/所属群体/家庭结构/教养方式/家庭气氛/成员文化程度/成员健康状况
-    # /家庭经济状况/成员不良行为/教师领导方式/教师教学风格/同伴接纳/大众传媒/社会文化社会风气/原因/对策
+    label_val = [[] for i in range(42)]
+    label_val[14] = '一般'
 
-    label_val = ['' for i in range(42)]
-    value_dic = {'label1': '身体攻击行为', 'label2': '言语攻击', 'label3': '间接攻击',
-                 'label4': '隐蔽性违纪行为', 'label5': '违反课堂制度行为', 'label6': '扰乱课堂秩序行为', 'label7': '违反校规校级行为',
-                 'label8': '欺骗行为', 'label9': '偷盗行为', 'label10': '背德行为',
-                 'label11': '强迫行为', 'label12': '沉迷行为',
-                 'label13': '言语性退缩', 'label14': '行为性退缩', 'label15': '心理性退缩',
-                 'label16': '抑郁情绪问题', 'label17': '抑郁行为问题', 'label18': '抑郁思维问题',
-                 'label19': '焦虑情绪问题', 'label20': '焦虑行为问题', 'label21': '焦虑思维问题',
-                 'label22': '学习能力问题', 'label23': '学习方法问题', 'label24': '学习态度问题', 'label25': '注意力问题',
-                 'label26': '自我吹嘘型问题', 'label27': '执拗型问题', 'label28': '自私型问题',
-                 'label29': '青春期问题', 'label30': '极端问题',
-                 'label31': '年级',
-                 'label32': '性别',
-                 'label33': '年龄',
-                 'label34': '生理疾病', 'label35': '心理疾病',
-                 'label36': '留守儿童', 'label37': '流动儿童', 'label38': '孤困儿童',  # 默认一般
-                 'label39': '寄养家庭', 'label40': '重组家庭', 'label41': '单亲家庭',
-                 'label42': '权威型教养方式', 'label43': '专制型教养方式', 'label44': '溺爱型教养方式', 'label45': '忽视型教养方式',
-                 'label46': '平静型家庭氛围', 'label47': '和谐型家庭氛围', 'label48': '冲突型家庭氛围', 'label49': '离散型家庭氛围',
-                 'label50': '父母生理疾病', 'label51': '父母心理疾病',
-                 'label52': '成员不良行为',  # 有或者无
-                 'label53': '成员文化程度',
-                 'label54': '家庭经济低收入', 'label55': '家庭经济高收入',
-                 'label56': '教师领导方式权威型', 'label57': '教师领导方式民主型', 'label58': '教师领导方式放任型',
-                 'label59': '同伴接纳被欢迎', 'label60': '同伴接纳一般', 'label61': '同伴接纳矛盾', 'label62': '同伴接纳被忽视', 'label63': '同伴接纳被拒绝',
-                 'label64': '大众传媒影响',
-                 'label65': '社会风气影响',
-                    #原因
-                 'label66': '情绪容易冲动',
-                 'label67': '认知方式过激',
-                 'label68': '病理性不自控',
-                 'label69': '安全感需求不满足',
-                 'label70': '友情需求不满足',
-                 'label71': '亲情需求不满足',
-                 'label72': '爱情需求不满足',
-                 'label73': '对他人尊重的不足',
-                 'label74': '被他人尊重的需求不满足',
-                 'label75': '被他人关注和重视的需求不满足',
-                 'label76': '自信心不足',
-                 'label77': '成就感需求不满足',
-                 'label78': '认知需求不平衡',
-                 'label79': '认知理解偏差',
-                 'label80': '无知模仿',
-                 'label81': '缺失教育引导',
-                #解决办法
-                 'label82': '说服教育法',
-                 'label83': '情感陶冶法',
-                 'label84': '榜样示范法',
-                 'label85': '自我教育法',
-                 'label86': '实践锻炼法',
-                 'label87': '品德评价法',
-                 'label88': '赏识教育法',
-                 'label8': '学业帮扶法',
-                 'label90': '家庭教育法',
-                 'label91': '与社区合作法',
-                 'label92': '志愿活动法',
-                 'label93': '在家学习法',
-                 'label94': '与家长沟通法',
-                 'label95': '参与决策法'
-
-                 }
     for i in range(len_data):
         label_value = label_list[i]
-        if i <1:
+        if i <13:
             print(i, label_value)
             p = re.compile(r'label\d{1,2}')
             q = re.compile(r'.+\w')
-            # print('sss',p.findall(label_value))
-            # print('sss',q.findall(label_value.split('\t')[2]))
-            label = p.findall(label_value)
-            val = q.findall(label_value.split('\t')[2])
-            #前面处理年龄性别年级信息，需要正则表达式规格化数据
-            # if label == 'label1':
-            #     label_value[0] = val
-            # elif label == 'label2':
-            #     label_value[0] = val
-            # elif label == 'label3':
-            #     label_value[0] = val
-            # elif label == 'label4':
-            #     label_value[1] = val
-
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label2')
-            label_value[0] = value_dic.get(label, 'label3')
-            label_value[1] = value_dic.get(label, 'label4')
-            label_value[1] = value_dic.get(label, 'label5')
-            label_value[1] = value_dic.get(label, 'label6')
-            label_value[1] = value_dic.get(label, 'label7')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
-            label_value[0] = value_dic.get(label, 'label1')
+            print('ss',p.findall(label_value))
+            # print('ssss',q.findall(label_value.split('\t')[2]))
+            label = p.findall(label_value)[0]
+            val = q.findall(label_value.split('\t')[2])[0]
 
 
+            print(label, type(label), val, type(val))
+            #前面处理基础信息，需要正则表达式规格化数据
+            if label == 'label31':  # 年级
+                print(val)
+                label_val[11] = val
+            if label == 'label32':  # 性别
+                print(val)
+                label_val[10] = val
+            if label == 'label33':  # 年龄
+                label_val[12] = val
 
+            if label == 'label64':  # 大众传媒影响
+                label_val[24] = val
+            if label == 'label65':  # 社会文化社会风气
+                label_val[25] = val
+
+            if label == 'label1':  # 攻击行为/
+                label_val[0].append('身体攻击行为')
+            if label == 'label2':
+                label_val[0].append( '言语攻击')
+            if label == 'label3':
+                label_val[0].append('间接攻击')
+
+            if label == 'label4':  # 违纪行为/
+                label_val[1].append('隐蔽性违纪行为')
+            if label == 'label5':
+                label_val[1].append('违反课堂制度行为')
+            if label == 'label6':
+                label_val[1].append('扰乱课堂秩序行为')
+            if label == 'label7':
+                label_val[1].append('违反校规校级行为')
+
+            if label == 'label8':  # 品行问题/
+                label_val[2].append('欺骗行为')
+            if label == 'label9':
+                label_val[2].append('偷盗行为')
+            if label == 'label10':
+                label_val[2].append('背德行为')
+
+            if label == 'label11':  # 不良嗜好/
+                label_val[3].append('强迫行为')
+            if label == 'label12':
+                label_val[3].append('沉迷行为')
+
+            if label == 'label13':  # 退缩/
+                label_val[4].append('言语性退缩')
+            if label == 'label14':
+                label_val[4].append('行为性退缩')
+            if label == 'label15':
+                label_val[4].append('心理性退缩')
+
+            if label == 'label16':  # 抑郁问题/
+                label_val[5].append('抑郁情绪问题')
+            if label == 'label17':
+                label_val[5].append('抑郁行为问题')
+            if label == 'label18':
+                label_val[5].append('抑郁思维问题')
+
+            if label == 'label19':  # 焦虑问题/
+                label_val[6].append('焦虑情绪问题')
+            if label == 'label20':
+                label_val[6].append('焦虑行为问题')
+            if label == 'label21':
+                label_val[6].append('焦虑思维问题')
+
+            if label == 'label26':   # 自我中心/
+                label_val[7].append('自我吹嘘型问题')
+            if label == 'label27':
+                label_val[7].append('执拗型问题')
+            if label == 'label28':
+                label_val[7].append('自私型问题')
+
+            if label == 'label22':    # 学习问题/
+                label_val[8].append('学习能力问题')
+            if label == 'label23':
+                label_val[8].append('学习方法问题')
+            if label == 'label24':
+                label_val[8].append('学习态度问题')
+            if label == 'label25':
+                label_val[8].append('注意力问题')
+
+            if label == 'label29': # 极端事件/
+                label_val[9].append('青春期问题')
+            if label == 'label30':
+                label_val[9].append('极端问题')
+
+            if label == 'label34':  # 健康状况/
+                label_val[13].append('生理疾病')
+            if label == 'label35':
+                label_val[13].append('心理疾病')
+
+            if label == 'label36':    # 所属群体/默认 一般
+                label_val[14].append('留守儿童')
+            if label == 'label37':
+                label_val[14].append('流动儿童')
+            if label == 'label38':
+                label_val[14].append('孤困儿童')
+
+            if label == 'label39':    # 家庭结构/
+                label_val[15].append('寄养家庭')
+            if label == 'label40':
+                label_val[15].append('重组家庭')
+            if label == 'label41':
+                label_val[15].append('单亲家庭')
+
+            if label == 'label42':    # 教养方式/
+                label_val[16].append('权威型教养方式')
+            if label == 'label43':
+                label_val[16].append('专制型教养方式')
+            if label == 'label44':
+                label_val[16].append('溺爱型教养方式')
+            if label == 'label45':
+                label_val[16].append('忽视型教养方式')
+
+            if label == 'label46':  # 家庭气氛
+                label_val[17].append('平静型家庭氛围')
+            if label == 'label47':
+                label_val[17].append('和谐型家庭氛围')
+            if label == 'label48':
+                label_val[17].append('冲突型家庭氛围')
+            if label == 'label49':
+                label_val[17].append('离散型家庭氛围')
+
+            if label == 'label53':  # 成员文化程度 如果标了认为是文化程度低，不标的为空
+                label_val[18].append('成员文化程度低')
+
+            if label == 'label50':  # 成员健康状况
+                label_val[19].append('父母生理疾病')
+            if label == 'label51':
+                label_val[19].append('父母心理疾病')
+
+            if label == 'label54':  # 家庭经济状况
+                label_val[20].append('家庭经济低收入')
+            if label == 'label55':
+                label_val[20].append('家庭经济高收入')
+
+            if label == 'label52':  # 成员不良行为 如果标了认为是存在不良行为，不标的默认为空
+                label_val[21].append('家庭成员存在不良行为')
+
+            if label == 'label56':    # /教师领导方式
+                label_val[22].append('教师领导方式权威型')
+            if label == 'label57':
+                label_val[22].append('教师领导方式民主型')
+            if label == 'label58':
+                label_val[22].append('教师领导方式放任型')
+
+            if label == 'label59':    # 同伴接纳
+                label_val[23].append('同伴接纳被欢迎')
+            if label == 'label60':
+                label_val[23].append('同伴接纳一般')
+            if label == 'label61':
+                label_val[23].append('同伴接纳矛盾')
+            if label == 'label62':
+                label_val[23].append('同伴接纳被忽视')
+            if label == 'label63':
+                label_val[23].append('同伴接纳被拒绝')
+
+            if label == 'label66':    # 根本原因
+                label_val[26].append('情绪容易冲动')
+            if label == 'label67':
+                label_val[26].append('认知方式过激')
+            if label == 'label68':
+                label_val[26].append('病理性不自控')
+            if label == 'label69':
+                label_val[26].append('安全感需求不满足')
+            if label == 'label70':
+                label_val[26].append('友情需求不满足')
+            if label == 'label71':
+                label_val[26].append('亲情需求不满足')
+            if label == 'label72':
+                label_val[26].append('爱情需求不满足')
+            if label == 'label73':
+                label_val[26].append('对他人尊重的不足')
+            if label == 'label74':
+                label_val[26].append('被他人尊重的需求不满足')
+            if label == 'label75':
+                label_val[26].append('被他人关注和重视的需求不满足')
+            if label == 'label76':
+                label_val[26].append('自信心不足')
+            if label == 'label77':
+                label_val[26].append('成就感需求不满足')
+            if label == 'label78':
+                label_val[26].append('认知需求不平衡')
+            if label == 'label79':
+                label_val[26].append('认知理解偏差')
+            if label == 'label80':
+                label_val[26].append('无知模仿')
+            if label == 'label81':
+                label_val[26].append('缺失教育引导')
+
+            # 育人对策
+            if label == 'label82':  # 根本原因
+                label_val[27].append('说服教育法')
+            if label == 'label83':
+                label_val[27].append('情感陶冶法')
+            if label == 'label84':
+                label_val[27].append('榜样示范法')
+            if label == 'label85':
+                label_val[27].append('自我教育法')
+            if label == 'label86':
+                label_val[27].append('实践锻炼法')
+            if label == 'label87':
+                label_val[27].append('品德评价法')
+            if label == 'label88':
+                label_val[27].append('赏识教育法')
+            if label == 'label89':
+                label_val[27].append('学业帮扶法')
+            if label == 'label90':
+                label_val[27].append('家庭教育法')
+            if label == 'label91':
+                label_val[27].append('与社区合作法')
+            if label == 'label92':
+                label_val[27].append('志愿活动法')
+            if label == 'label93':
+                label_val[27].append('在家学习法')
+            if label == 'label94':
+                label_val[27].append('与家长沟通法')
+            if label == 'label95':
+                label_val[27].append('参与决策法')
+
+            print(label_val)
 
 
 if __name__ == "__main__":
